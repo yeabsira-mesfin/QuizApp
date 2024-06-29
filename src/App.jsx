0a08d3762f18fac4cae79627d49c0f1a853c5fe8
yeabsira_mesfin <yabidesta@gmail.com>
2024-06-29 08:27:56 -0400

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
-function App() {}
+import React from 'react'
+import Header from './components/Header'
+const App = () => {
+  return (
+    <>
+    <Header/> 
+    </>
+  )
+}
 
-export default App;
+export default App
