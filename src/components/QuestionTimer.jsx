71face195072e43dd9ea1b20aa4b94366cdde51c
yeabsira_mesfin <yabidesta@gmail.com>
2024-06-29 11:14:00 -0400

    Timer to the quesitons

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+
+const QuestionTimer = ({ timeout, onTimeout }) => {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect(() => {
+    setTimeout(onTimeout, timeout);
+  }, [timeout, onTimeout]);
+
+
+  useEffect(() => {
+    setInterval(() => {
+      setRemainingTime((prevRemaingTime => prevRemaingTime - 10));
+    }, 100);
+  }, []);
+
+  return <progress id="question-time" max={timeout} value={remainingTime}/>;
+};
+
+export default QuestionTimer;
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,42 +1,46 @@
 import React, { useState } from "react";
 import QUESTIONS from "../questions";
-import quizCompleted from '../assets/quiz-complete.png'
+import QuestionTimer from "./QuestionTimer";
+import quizCompleted from "../assets/quiz-complete.png";
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
 
-  function handleSelectAnswer(selectedAnswer){
-    setUserAnswers((prevAnswer)=>{
-        return [...prevAnswer, selectedAnswer]
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevAnswer) => {
+      return [...prevAnswer, selectedAnswer];
     });
   }
 
-
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  if(quizIsComplete){
-    return <div id="summary">
-        <img src= {quizCompleted} alt="Trophy" />
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleted} alt="Trophy" />
         <h2>Quiz Completed</h2>
-    </div>
+      </div>
+    );
   }
 
-  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
-  shuffledAnswers.sort(()=> Math.random() - 0.5); // Used to shuffle the answers
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5); // Used to shuffle the answers
 
   return (
     <div id="quiz">
-       <div id="question">
-          <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-          <ul id="answers">
-            {shuffledAnswers.map((answer) => (
-              <li key={answer} className="answer">
-        
-                <button onClick={()=>handleSelectAnswer(answer)}>{answer}</button>
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div id="question">
+        <QuestionTimer timeout={10000} onTimeout={()=> handleSelectAnswer(null)}/>
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {shuffledAnswers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
