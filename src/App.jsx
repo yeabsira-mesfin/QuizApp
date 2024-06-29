a9de3097ebefca23afc3ebcab728ce3fbbe93808
yeabsira_mesfin <yabidesta@gmail.com>
2024-06-29 09:17:36 -0400

    Quiz answers displayed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
+import Quiz from './components/Quiz'
 const App = () => {
   return (
     <>
     <Header/> 
+   
+    <main>
+    <Quiz/>
+    </main>
     </>
   )
 }
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import QUESTIONS from "../questions";
 const Quiz = () => {
-  return (
-    <div>Quiz</div>
-  )
-}
+  const [userAnswers, setUserAnswers] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
 
-export default Quiz
+  function handleSelectAnswer(selectedAnswer){
+    setUserAnswers((prevAnswer)=>{
+        return [...prevAnswer, selectedAnswer]
+    });
+  }
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
+  shuffledAnswers.sort((a,b)=> 1);
+
+  return (
+    <div id="quiz">
+        <div id="question">
+          <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+          <ul id="answers">
+            {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+              <li key={answer} className="answer">
+        
+                <button onClick={()=>handleSelectAnswer(answer)}>{answer}</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+    </div>
+  );
+};
+
+export default Quiz;
