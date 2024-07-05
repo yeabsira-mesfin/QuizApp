30cd75efd0692f4506cb3b36052ec62a6892d302
yeabsira_mesfin <yabidesta@gmail.com>
2024-07-05 11:37:19 -0400

    Correct answer,wrong answer and skipped answers actual numbers

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -17,7 +17,7 @@ const QuestionTimer = ({ timeout, onTimeout,mode }) => {
   useEffect(() => {
     console.log('setting interval')
    const interval = setInterval(() => {
-      setRemainingTime((prevRemaingTime => prevRemaingTime - 50));
+      setRemainingTime((prevRemaingTime => prevRemaingTime - 100));
     }, 100);
 
    return ()=>{clearInterval(interval)}
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,67 +1,97 @@
 import React from "react";
 import QUESTION from "../questions";
 import quizCompleted from "../assets/quiz-complete.png";
-import tryAgain from '../assets/TryAgain.png'
+import tryAgain from "../assets/TryAgain.png";
 const Summary = ({ userAnswers }) => {
-    const skippedAnswers = userAnswers.filter(answer=> answer === null);
-    const correctAnswewrs = userAnswers.filter((answer,index)=>answer === QUESTION[index].answers[0])
-
-    const SkippedAnswerPercent = Math.round((skippedAnswers.length / userAnswers.length) * 100);
-    const correctAnswerPercent = Math.round((correctAnswewrs.length / userAnswers.length) * 100);
-    const wrongAnswerPercent = Math.round( 100 - SkippedAnswerPercent - correctAnswerPercent);
-
-    let winStatment = '';
-    let winStatmentClass = '';
-
-    if(correctAnswerPercent>wrongAnswerPercent){
-        winStatment = "You won!"
-        winStatmentClass = "winStatmentWin";
-    } else if(correctAnswerPercent< wrongAnswerPercent){
-        winStatment = "You Lost!"
-        winStatmentClass = "winStatmentLose";
-    } else{
-        winStatment = 'You didn\'t answer the whole question'
-        winStatmentClass = "winStatmentSkipped";
+ 
+  let skippedA = 0;
+const skippedAnswers = userAnswers.filter((answer) => {
+  if (answer === null) {
+    skippedA++;
+    return true;
+  }
+  return false;
+});
+let correctA = 0;
+  const correctAnswewrs = userAnswers.filter(
+    (answer, index) => {
+        if(answer === QUESTION[index].answers[0]){
+            answer === QUESTION[index].answers[0]
+            correctA++
+            return true
+        }
     }
+  );
+
+let wrongA = 7 - skippedA - correctA;
+
+  const SkippedAnswerPercent = Math.round(
+    (skippedAnswers.length / userAnswers.length) * 100
+  );
+  const correctAnswerPercent = Math.round(
+    (correctAnswewrs.length / userAnswers.length) * 100
+  );
+  const wrongAnswerPercent = Math.round(
+    100 - SkippedAnswerPercent - correctAnswerPercent
+  );
+
+  let winStatment = "";
+  let winStatmentClass = "";
+
+  if (correctAnswerPercent > wrongAnswerPercent) {
+    winStatment = "You won!";
+    winStatmentClass = "winStatmentWin";
+  } else if (correctAnswerPercent < wrongAnswerPercent) {
+    winStatment = "You Lost!";
+    winStatmentClass = "winStatmentLose";
+  } else {
+    winStatment = "You didn't answer the whole question";
+    winStatmentClass = "winStatmentSkipped";
+  }
   return (
     <div id="summary">
-      <img src={correctAnswerPercent>wrongAnswerPercent? quizCompleted : tryAgain} alt="Trophy" />
+      <img
+        src={
+          correctAnswerPercent > wrongAnswerPercent ? quizCompleted : tryAgain
+        }
+        alt="Trophy"
+      />
       <h2>Quiz Completed</h2>
       <div id="summary-stats">
         <p>
           <span className="number">{correctAnswerPercent}%</span>
-          <span className="text">Correctly Answered</span>
+          <span className="text">{correctA}Correctly Answered</span>
         </p>
         <p>
           <span className="number">{wrongAnswerPercent}%</span>
-          <span className="text">Incorrecty Answered</span>
+          <span className="text">{wrongA}Incorrecty Answered</span>
         </p>
         <p>
           <span className="number">{SkippedAnswerPercent}%</span>
-          <span className="text">Skipped</span>
+          <span className="text">{skippedA}Skipped</span>
         </p>
       </div>
       <ol>
-        {userAnswers.map((answer,index) => {
-            let cssClass = 'user-answer'
-            if(answer ===null){
-                cssClass+= ' skipped'
-            } else if(answer === QUESTION[index].answers[0]){
-                cssClass += ' correct'
-            } else {
-                cssClass += ' wrong'
-            }
+        {userAnswers.map((answer, index) => {
+          let cssClass = "user-answer";
+          if (answer === null) {
+            cssClass += " skipped";
+          } else if (answer === QUESTION[index].answers[0]) {
+            cssClass += " correct";
+          } else {
+            cssClass += " wrong";
+          }
           return (
             <li key={index}>
               <h3>{index + 1}</h3>
               <p className="question">{QUESTION[index].text}</p>
-              <p className={cssClass}>{answer?? 'Skipped'}</p>
+              <p className={cssClass}>{answer ?? "Skipped"}</p>
             </li>
           );
         })}
       </ol>
       <h1 className={winStatmentClass}>{winStatment}</h1>
-     
+     <h3>{},{correctA},{wrongA}</h3>
     </div>
   );
 };
