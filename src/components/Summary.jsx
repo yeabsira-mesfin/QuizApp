4f89ec7353e07c85fc6bcd288a8d156c4813d0f3
yeabsira_mesfin <yabidesta@gmail.com>
2024-07-05 04:46:13 -0400

    Percentage for skipped, correct and incorrect answers

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -2,21 +2,27 @@ import React from "react";
 import QUESTION from "../questions";
 import quizCompleted from "../assets/quiz-complete.png";
 const Summary = ({ userAnswers }) => {
+    const skippedAnswers = userAnswers.filter(answer=> answer === null);
+    const correctAnswewrs = userAnswers.filter((answer,index)=>answer === QUESTION[index].answers[0])
+
+    const SkippedAnswerPercent = Math.round((skippedAnswers.length / userAnswers.length) * 100);
+    const correctAnswerPercent = Math.round((correctAnswewrs.length / userAnswers.length) * 100);
+    const wrongAnswerPercent = Math.round( 100 - SkippedAnswerPercent - correctAnswerPercent);
   return (
     <div id="summary">
       <img src={quizCompleted} alt="Trophy" />
       <h2>Quiz Completed</h2>
       <div id="summary-stats">
         <p>
-          <span className="number">10%</span>
+          <span className="number">{SkippedAnswerPercent}%</span>
           <span className="text">Skipped</span>
         </p>
         <p>
-          <span className="number">10%</span>
+          <span className="number">{correctAnswerPercent}%</span>
           <span className="text">Correctly Answered</span>
         </p>
         <p>
-          <span className="number">10%</span>
+          <span className="number">{wrongAnswerPercent}%</span>
           <span className="text">Incorrecty Answered</span>
         </p>
       </div>
